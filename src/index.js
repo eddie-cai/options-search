e3378659482a966ce6c3a8b78728c5760cf3d0f5
eddie-cai <edmund.cai@octanelending.com>
2022-05-28 18:55:19 -0400

    fixed webpack

--- src/index.js
+++ src/index.js
@@ -6,9 +6,19 @@ import SearchBar from './components/SearchBar';
 class App extends React.Component {
     handleTermChange(term) {
         const request = require('request');
-        const url = `https://finance.yahoo.com/quote/${term}/options?p=${term}`
+        const url = `https://query1.finance.yahoo.com/v7/finance/options${term}`
+        let options = {json: true};
+        request(url, options, (error, res, body) => {
+            if (error) {
+                return  console.log(error)
+            };
 
-        
+            if (!error && res.statusCode == 200) {
+                // do something
+                // with JSON, using the 'body' variable
+                console.log(body)
+            };
+        });
     }
 
     render() {
